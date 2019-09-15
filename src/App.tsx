8d87634a946a359e7cf4e87fff59e1b2a56ea6d3
Hugo González <pinelo93@gmail.com>
2019-09-14 20:53:05 -0500

    Prettify

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ import Dashboard from "./components/dashboard";
 import Form from "./components/form";
 import { Query } from "react-apollo";
 import gql from "graphql-tag.macro";
-import moment from 'moment-timezone'
+import moment from "moment-timezone";
 
-let today = moment(new Date().toISOString()).tz('America/Monterrey').format()
+let today = moment(new Date().toISOString())
+  .tz("America/Monterrey")
+  .format();
 
 const GET_CONSUMPTION = gql`
   query getConsumption($userQuery: UserQuery, $intakeQuery: DateRange) {
@@ -33,25 +35,33 @@ const GET_CONSUMPTION = gql`
   }
 `;
 
-
-
 interface Session {
   userId: number | null;
 }
 
-const SessionContext = React.createContext<Session>({userId: null})
+const SessionContext = React.createContext<Session>({ userId: null });
 
 const App = () => {
-  const [formActive, setFormActive] = useState(false)
-  
+  const [formActive, setFormActive] = useState(false);
+
   return (
-  <SessionContext.Provider value={{userId: 1}}>
-    <div className="App">
+    <SessionContext.Provider value={{ userId: 1 }}>
+      <div className="App">
         <div id="main__btn-section" className="grid-1 row-gap-10">
-          <div className="main-btn button btn--blue" onClick={ () => setFormActive(!formActive) }>Add Intake</div>
-          <div className="main-btn button btn--blue" onClick={ () => setFormActive(!formActive) }>Daily Log</div>
+          <div
+            className="main-btn button btn--blue"
+            onClick={() => setFormActive(!formActive)}
+          >
+            Add Intake
+          </div>
+          <div
+            className="main-btn button btn--blue"
+            onClick={() => setFormActive(!formActive)}
+          >
+            Daily Log
+          </div>
         </div>
-        <Form active={formActive} close={e => setFormActive(false)}/>
+        <Form active={formActive} close={e => setFormActive(false)} />
         <div className="container--80">
           <Query
             query={GET_CONSUMPTION}
@@ -67,14 +77,18 @@ const App = () => {
             }}
           </Query>
         </div>
-        <div className='flex-col'>
-          <div style={{padding: '1rem', background: "#000a"}}>
-            <a style={{color: 'white'}} href="https://ods.od.nih.gov/Health_Information/Dietary_Reference_Intakes.aspx">Daily Recommended Intakes</a>
+        <div className="flex-col">
+          <div style={{ padding: "1rem", background: "#000a" }}>
+            <a
+              style={{ color: "white" }}
+              href="https://ods.od.nih.gov/Health_Information/Dietary_Reference_Intakes.aspx"
+            >
+              Daily Recommended Intakes
+            </a>
           </div>
         </div>
       </div>
-  </SessionContext.Provider>
-    
+    </SessionContext.Provider>
   );
 };
 export default App;
